ed19a3c4c7d104fdb66983b8a6eec959bc58c6c0
AlexTechFlow <alextechflower@gmail.com>
2018-01-26 10:24:07 -0500

    Upd

---
+++ web-design-class-2/project1/script.js
@@ -0,0 +1,9 @@
+var names = ["Joe", "Fraser", "Ali"];
+
+var limit = 10;
+
+// for loop for repeating things over and over again
+// i<10; this line is for controling the loop
+for (var i=0; i<10; i ++){
+	console.log(i);
+}
