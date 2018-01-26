7a1ad441a4cdf421b858f188a5b878226ace2502
AlexTechFlow <alextechflower@gmail.com>
2018-01-26 11:15:19 -0500

    Upd

--- web-design-class-2/project1/script.js
+++ web-design-class-2/project1/script.js
@@ -1,15 +1,18 @@
 var names = ["Joe", "Fraser", "Ali", "Lisa", "UT"];
-var grades = [50, 75, 12"];
+var grades = [50, 75, 12];
 var mahBools = [true, false];
 
-function printNames(namesList){
-	var limit = names.length; // DYNAMIC CODE example
+function printNames(list){ // namesList is just a variable to use inside the function
+	var limit = list.length; // DYNAMIC CODE example
 
-	for (var i=0; i<limit; i ++){
-	console.log(names[i]);
+	for (var i=0; i<limit; i++){
+	console.log(list[i] + " FROM FUNCTION");
     }
 }
 
+printNames(names);
+printNames(grades);
+printNames(mahBools);
 
 
 // CLASS NOTES:
@@ -21,3 +24,4 @@ function printNames(namesList){
 
 // A function is an independent piece of code. We sometimes need to send information to it. We do it by passing it parameters function(parameters){};
 // So function is like a DVD player. You need to insert a disc or it won't play anything.
+//But the function won't do anything unless you actually USE IT.
