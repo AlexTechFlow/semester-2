18f71f560dc9b2d4c5ce10386837689a78908e78
AlexTechFlow <alextechflower@gmail.com>
2018-01-26 11:03:49 -0500

    Upd

--- web-design-class-2/project1/script.js
+++ web-design-class-2/project1/script.js
@@ -1,11 +1,23 @@
 var names = ["Joe", "Fraser", "Ali", "Lisa", "UT"];
+var grades = [50, 75, 12"];
+var mahBools = [true, false];
 
-var limit = names.length; // DYNAMIC CODE example
+function printNames(namesList){
+	var limit = names.length; // DYNAMIC CODE example
+
+	for (var i=0; i<limit; i ++){
+	console.log(names[i]);
+    }
+}
+
+
+
+// CLASS NOTES:
 
 // for loop for repeating things over and over again
 // i<10; this line is for controling the loop
-for (var i=0; i<limit; i ++){
-	console.log(names[i]);
-}
 
 // In HTML, common array elements could be list items, divs
+
+// A function is an independent piece of code. We sometimes need to send information to it. We do it by passing it parameters function(parameters){};
+// So function is like a DVD player. You need to insert a disc or it won't play anything.
