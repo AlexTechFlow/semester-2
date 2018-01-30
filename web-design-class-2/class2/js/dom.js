e13c9f28a7591225744ccf162a4915e885066a83
AlexTechFlow <alextechflower@gmail.com>
2018-01-30 12:25:30 -0500

    DOM & JS - Exercise

---
+++ web-design-class-2/class2/js/dom.js
@@ -0,0 +1,9 @@
+console.log("Hi");
+
+var targetP = document.getElementById("targetP");
+console.log(targetP);
+
+targetP.innerHTML = "Updated via JavaScript";
+
+var targetLis = document.getElementsByClassName("targetLi"); // elements means targeting many items
+console.log(targetLis);
