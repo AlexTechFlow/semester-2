91b8a9b6cdf6dd0bd6ff7b38b966b7961d8a78d9
AlexTechFlow <alextechflower@gmail.com>
2018-01-30 11:51:15 -0500

    Upd

--- web-design-class-2/class2/js/myLibrary.js
+++ web-design-class-2/class2/js/myLibrary.js
@@ -29,14 +29,19 @@ var myLibrary = {
 
 
 /// EXERCISE IN-CLASS //
+// 1.
 //Create JavaScript function that:
 // accepts a library object and 
 // returns an array of books
 
+// 2.
 // Outside of that function, create another function that:
 // accepts an array of books and
 // console.logs the titles of those books		
 
+
+
+//1.
 function getBooks(libraryObject){ // name function in reference to what it does
 	//parameter is a generic libray object because we may have many different libraries
 	// the function will find what you need with this syntax - any array of books
@@ -45,6 +50,23 @@ function getBooks(libraryObject){ // name function in reference to what it does
 
 var receivedBooks = getBooks(myLibrary);
 console.log(receivedBooks);
+
+
+
+// 2.
+function logTitles(bookArray){ // arrays and for loops go hand in hand
+	for(i=0; i<bookArray.length; i++){
+		console.log(i);
+		console.log(bookArray[i]);
+
+	}
+
+}
+
+logTitles(receivedBooks);
+
+
+
 
 // NAMING OF FUNCTIONS AND VARS = WHAT THEY DO
 // NAMING IS IMPORTANT
@@ -58,12 +80,14 @@ function logBookTitles(){};
 
 // Examples: input processing and output
 
-function teach(courseName){
-	prepareStuff();
-	teachStuff();
-	gradeStuff();
+// function teach(courseName){
+// 	prepareStuff();
+// 	teachStuff();
+// 	gradeStuff();
 
-	return grades;
-}
+// 	return grades;
+// }
 
-var gradesToDean = teach("IMM");
+// var gradesToDean = teach("IMM");
+
+
