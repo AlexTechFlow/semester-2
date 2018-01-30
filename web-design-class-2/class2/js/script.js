e9af4f45bd31f7a6f0ad7331a3d0461c549e3d6e
AlexTechFlow <alextechflower@gmail.com>
2018-01-30 09:44:35 -0500

    New Class - JS Objects

---
+++ web-design-class-2/class2/js/script.js
@@ -0,0 +1,32 @@
+console.log("I am connected");
+
+var o = {}; // make a new object
+
+o = {
+
+	firstName:"Wasim",
+	lastName:"Singh", 
+	age:30,
+	wight:300,
+	likePizza:true,
+	gamesPlayed: ["Starcraft", "CS:GO"], // array inside object
+	address:{ // property that is an object
+		street:"12 Fake Street",
+		city:"Springfield",
+		postalCode:"L9K0L9"
+	}
+
+}
+
+// objects are descriptive, arrays are not
+
+var lionJo = {
+	length:20,
+	wight:300,
+	fierceFactor:"VERY FIERCE"; // all thes are object properties 
+}
+
+var pride = [lionJo, lionPjil, lionDanZen];
+
+// USE ARRAYS FOR SETS OF THINGS
+// USE OBJECTS TO DESCRIBE THINGS
