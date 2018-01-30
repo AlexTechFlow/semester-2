64283fe0d50086f2061371c7e797c824a7e52f3f
AlexTechFlow <alextechflower@gmail.com>
2018-01-30 10:14:21 -0500

    Upd

---
+++ web-design-class-2/class2/js/myLibrary.js
@@ -0,0 +1,27 @@
+var myLibrary = {
+			name:"Oakville Public Library",
+			services:["Borrowing", "Internet", "Meeting Rooms", "Comunity"],
+			address:{
+                        street:"120 Navy St.",
+                        town:"Oakville",
+                        province:"Ontario",
+                        postalCode:"L6J 2Z4"
+              		},
+			books:[
+					{
+						"pages":"198", 
+						"author":"Neil Young", 
+						"title":"Waging Heavy Peace"
+					},
+    				{
+						"pages":"1", 
+						"author":"Matthew Inman", 
+						"title":"How to Tell If Your Cat Is Plotting to Kill You"
+					},
+					{
+						"pages":"234", 
+						"author":"Tyler Hamilton", 
+						"title":"The Secret Race"
+					}
+				],
+			};
--- web-design-class-2/class2/js/script.js
+++ web-design-class-2/class2/js/script.js
@@ -2,21 +2,24 @@ console.log("I am connected");
 
 var o = {}; // make a new object
 
-o = {
+o = { // this is JavaScript object notation
 
-	firstName:"Wasim",
-	lastName:"Singh", 
-	age:30,
-	wight:300,
-	likePizza:true,
-	gamesPlayed: ["Starcraft", "CS:GO"], // array inside object
-	address:{ // property that is an object
-		street:"12 Fake Street",
-		city:"Springfield",
-		postalCode:"L9K0L9"
+	"firstName":"Wasim", // for JSON, you ned to have strings encased in double quotes: https://jsonformatter.curiousconcept.com/ if you want to use/transfer this file as JSON; FOR js this is not required
+	"lastName":"Singh", 
+	"age":30,
+	"wight":300,
+	"likePizza":true,
+	"gamesPlayed": ["Starcraft", "CS:GO"], // array inside object
+	"address":{ // property that is an object
+		"street":"12 Fake Street",
+		"city":"Springfield",
+		"postalCode":"L9K0L9"
 	}
 
-}
+};
+
+// VALIDATE YOUR JSON AS SOON AS YOU HAVE ACCESS TO IT TO MAKE DEBUGGING EASIER
+
 
 // objects are descriptive, arrays are not
 
@@ -26,7 +29,10 @@ var lionJo = {
 	fierceFactor:"VERY FIERCE" // all thes are object properties 
 };
 
-var pride = [lionJo];
+var lionPjil;
+var lionDanZen;
+
+var pride = [lionJo, lionPjil, lionDanZen];
 
 // USE ARRAYS FOR SETS OF THINGS
 // USE OBJECTS TO DESCRIBE THINGS
@@ -42,3 +48,11 @@ console.log(o.gamesPlayed);
 console.log(o.gamesPlayed[1]);
 console.log(o.address);
 console.log(o.address.postalCode);
+
+//set values
+o.address.postalCode = "Y6Y6Y6";
+o.gamesPlayed.push("Animal Crossing"); // push this to the gamesPlayed
+
+// Figure out how things are structured, how to read info.
+
+// JSON - JavaScript object notation
